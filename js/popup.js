1a79ffd683ac7b6e5d900fb54a56d6c1f1affab5
高月明 <gaoym@mingyuanyun.com>
2017-11-30 18:37:01 +0800

    增加自动复制功能

--- js/popup.js
+++ js/popup.js
@@ -3,20 +3,21 @@ document.addEventListener(
   () => {
     const $input = document.querySelector("#input")
     const $output = document.querySelector("#output")
+    const $btn = document.querySelector("#btn")
+
+    new Clipboard($btn)
 
     const upperCase = str => str.replace(/[a-z]/g, e => e.toLocaleUpperCase())
-
     const replace = str => str.replace(/\d{2,}/g, e => Number(e).toString(16))
 
-    const handler = e => {
-      if (e.code === "Enter" || e.keyCode === 13) {
+    $input.addEventListener("keydown", (e) => {
+      if (e.keyCode === 13) {
         const val = $input.value.trim()
         if (!val) return
         $output.innerText = replace(md5(upperCase(val))).substr(-12)
+        $btn.click()
       }
-    }
-
-    $input.addEventListener("keydown", handler, false)
+    }, false)
   },
   false
 )
